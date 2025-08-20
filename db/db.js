293ead974a5359ab370984f73a6b804c687d78e5
webcomercialwposs-max <webcomercialwposs@gmail.com>
2025-08-20 17:21:35 -0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,31 +1,20 @@
 // db/db.js
 const { Pool } = require('pg');
-
-// Si existe la variable DATABASE_URL, úsala. De lo contrario, usa las variables locales.
-const connectionString = process.env.DATABASE_URL || {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-};
+require('dotenv').config();
 
 const pool = new Pool({
-    connectionString,
-    ssl: {
-        rejectUnauthorized: false, // Necesario para algunas configuraciones de bases de datos
-    },
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('connect', () => {
-    console.log('Conectado a la base de datos PostgreSQL');
+  console.log('Conectado a la base de datos PostgreSQL');
 });
 
 pool.on('error', (err) => {
-    console.error('Error inesperado en la conexión a la base de datos', err);
-    process.exit(-1);
+  console.error('Error inesperado en la conexión a la base de datos', err);
+  process.exit(-1);
 });
 
 module.exports = {
-    pool,
+  pool,
 };
